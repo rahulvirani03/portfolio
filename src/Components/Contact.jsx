caf5f9bf8e1577b094ee4e066bc95fdc916d6075
rahulvirani03 <rahulsvirani25@gmail.com>
2022-07-12 16:50:37 +0530

    resume added

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -15,77 +15,71 @@ export const Contact = () => {
 
       <div className="center-container">
         <div className="contact-item-container">
-          <div className="item-container">
-            <div
-              onClick={() => openInNewTab("https://github.com/rahulvirani03")}
-              className="contact-item github"
-            >
+          <div
+            className="item-container"
+            onClick={() => openInNewTab("https://github.com/rahulvirani03")}
+          >
+            <div className="contact-item github">
               <img src={github} alt="github" />
             </div>
             <p className="name">rahulvirani03</p>
           </div>
           <div className="item-line"></div>
-          <div className="item-container">
-            <div
-              onClick={() =>
-                openInNewTab(
-                  " https://www.linkedin.com/in/rahul-virani-797416181/"
-                )
-              }
-              className="contact-item linkedin"
-            >
+          <div
+            className="item-container"
+            onClick={() =>
+              openInNewTab(
+                " https://www.linkedin.com/in/rahul-virani-797416181/"
+              )
+            }
+          >
+            <div className="contact-item linkedin">
               <img src={linkedin} alt="linkedin" />
             </div>
             <p className="name">rahul virani</p>
           </div>
           <div className="item-line"></div>
-          <div className="item-container">
-            <div
-              onClick={() =>
-                openInNewTab(
-                  "mailto:rahulsvirani25@gmail.com?subject=Enquiry&body=Hello!"
-                )
-              }
-              className="contact-item"
-            >
+          <div
+            className="item-container"
+            onClick={() =>
+              openInNewTab(
+                "mailto:rahulsvirani25@gmail.com?subject=Enquiry&body=Hello!"
+              )
+            }
+          >
+            <div className="contact-item">
               <img src={mail} alt="mail" />
             </div>
 
             <p className="name">rahulsvirani25@gmail.com</p>
           </div>
           <div className="item-line"></div>
-          <div className="item-container">
-            <div
-              onClick={() => openInNewTab("https://twitter.com/rahul_virani03")}
-              className="contact-item twitter"
-            >
+          <div
+            className="item-container"
+            onClick={() => openInNewTab("https://twitter.com/rahul_virani03")}
+          >
+            <div className="contact-item twitter">
               <img src={twitter} alt="twitter" />
             </div>
             <p className="name">rahul_virani03</p>
           </div>
           <div className="item-line"></div>
-          <div className="item-container">
-            <div
-              onClick={() =>
-                openInNewTab("https://instagram.com/rahul_virani03")
-              }
-              className="contact-item insta"
-            >
+          <div
+            className="item-container"
+            onClick={() => openInNewTab("https://instagram.com/rahul_virani03")}
+          >
+            <div className="contact-item insta">
               <img src={instagram} alt="insta" />
             </div>
             <p className="name">rahul_virani03</p>
           </div>
         </div>
         <div className="download-button-container">
-          <a href="../../Resume-Final.pdf" download>
-            <button
-              className="download-button"
-              // onClick={() => {
-              //   window.open("../../Resume-Final.pdf");
-              // }}
-            >
-              Download Resume
-            </button>
+          <a
+            href="https://rahul-virani-portfolio.herokuapp.com/resume-download"
+            download
+          >
+            <button className="download-button">Download Resume</button>
           </a>
         </div>
       </div>
